5644a89a827fec31f9d9ca643a0da8cf19813f25
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-02-16 14:45:02 +0100

    fixing setup script

--- scripts/src/setup.ts
+++ scripts/src/setup.ts
@@ -62,7 +62,8 @@ const setupMint = async (
     network: string,
     connection: Connection,
     clientKeypair: Signer,
-    mintTokens: boolean
+    mintTokens: boolean,
+    mintToAccount?: string
 ) => {
     console.log(`Creating Mint ${name}...`);
     let mintKeyPair: Keypair;
@@ -85,13 +86,13 @@ const setupMint = async (
         writeSecretKey(mintKeyPair.secretKey, "mint", network);
     }
     // For Devnet [ Create Token Accounts and Mint Tokens ]
-    if (mintTokens) {
+    if (mintTokens && mintToAccount) {
         console.log('Mintinggggg')
         const associatedToken = await getOrCreateAssociatedTokenAccount(
             connection,
             clientKeypair,
             mintKeyPair.publicKey,
-            new PublicKey("Fr1vWrS8YYFVjHx9UkzL5gFCUSP2tVBH3aGSK9ZTi7LP")
+            new PublicKey(mintToAccount)
         )
         await mintTo(
             connection,
@@ -99,7 +100,7 @@ const setupMint = async (
             mintKeyPair.publicKey,
             associatedToken.address,
             clientKeypair,
-            2000*10**TOKEN_DECIMALS
+            20000*10**TOKEN_DECIMALS
         )
         console.log('Done minting for:', associatedToken.address)
     }
@@ -113,6 +114,7 @@ const setup = async (
     normalStakingApy: number,
     lockedStakingApy: number,
     earlyWithdrawalFee: number,
+    runInit: boolean = true
 ) => {
     let rpc;
     if (network == "devnet") {
@@ -139,81 +141,82 @@ const setup = async (
             network,
             connection,
             adminKeyPair,
-            true
+            true,
+            "4MBmCpddAHFqC8Fkj1DZ4RM8JoYXWdXjHQ9LXxgiUped"
         )
     }
-    const mintPubkey = getPublicKey("mint", network);
-    const [dataAccountPubKey, ] = PublicKey.findProgramAddressSync(
-        [Buffer.from("spl_staking", "utf-8"), adminKeyPair.publicKey.toBuffer(), mintPubkey.toBuffer()],
-        programId
-    );
-    const [dataAccount,] = PublicKey.findProgramAddressSync(
-        [Buffer.from("spl_staking", "utf-8"), adminKeyPair.publicKey.toBuffer(), mintPubkey.toBuffer()],
-        programId
-    );
-    const createTokenAcctIX = SystemProgram.createAccount({
-        programId: TOKEN_PROGRAM_ID,
-        space: AccountLayout.span,
-        lamports: await connection.getMinimumBalanceForRentExemption(AccountLayout.span),
-        fromPubkey: adminKeyPair.publicKey,
-        newAccountPubkey: tokenAccountKeypair.publicKey
-    })
-    const initTokenAcctIX = createInitializeAccountInstruction(
-        tokenAccountKeypair.publicKey,
-        mintPubkey,
-        adminKeyPair.publicKey
-    );
-    const instructionData = Buffer.from(
-        Uint8Array.of(
-            0,
-            ...new BN(minimumStakeAmount * 10**TOKEN_DECIMALS).toArray("le", 8),
-            ...new BN(minimumLockDuration).toArray("le", 8),
-            ...new BN(normalStakingApy * 10).toArray("le", 8),
-            ...new BN(lockedStakingApy * 10).toArray("le", 8),
-            ...new BN(earlyWithdrawalFee * 10).toArray("le", 8)
-        )
-    )
-    const initIX = new TransactionInstruction({
-        programId,
-        keys: [
-            { pubkey: adminKeyPair.publicKey, isSigner: true, isWritable: false },
-            { pubkey: dataAccountPubKey, isSigner: false, isWritable: true },
-            { pubkey: tokenAccountKeypair.publicKey, isSigner: false, isWritable: true },
-            { pubkey: mintPubkey, isSigner: false, isWritable: false },
-            { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
-            { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
-            { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
-        ],
-        data: instructionData
-    });
-    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
-    const messageV0 = new TransactionMessage({
-        payerKey: adminKeyPair.publicKey,
-        recentBlockhash: latestBlockHash.blockhash,
-        instructions: [createTokenAcctIX, initTokenAcctIX, initIX]
-    }).compileToV0Message();
-    const transaction = new VersionedTransaction(messageV0);
-    transaction.sign([adminKeyPair, tokenAccountKeypair]);
-    const txId = await connection.sendTransaction(transaction, {maxRetries: 5});
-    const confirmation = await connection.confirmTransaction({
-        signature: txId,
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
-    });
-    if (confirmation.value.err) {
-        console.log(`Transaction could not be confirmed ❌❌❌`)
-    } else {
-        writePublicKey(tokenAccountKeypair.publicKey, 'token_acct', network);
-        writeSecretKey(tokenAccountKeypair.secretKey, 'token_acct', network);
-        writePublicKey(dataAccountPubKey, 'data_acct', network);
-        await mintTo(
-            connection,
-            adminKeyPair,
-            mintPubkey,
+    if (runInit) {
+        const mintPubkey = getPublicKey("mint", network);
+        const [dataAccountPubKey, ] = PublicKey.findProgramAddressSync(
+            [Buffer.from("spl_staking", "utf-8"), adminKeyPair.publicKey.toBuffer(), mintPubkey.toBuffer()],
+            programId
+        );
+        const createTokenAcctIX = SystemProgram.createAccount({
+            programId: TOKEN_PROGRAM_ID,
+            space: AccountLayout.span,
+            lamports: await connection.getMinimumBalanceForRentExemption(AccountLayout.span),
+            fromPubkey: adminKeyPair.publicKey,
+            newAccountPubkey: tokenAccountKeypair.publicKey
+        })
+        const initTokenAcctIX = createInitializeAccountInstruction(
             tokenAccountKeypair.publicKey,
-            adminKeyPair,
-            100000*10**TOKEN_DECIMALS
+            mintPubkey,
+            adminKeyPair.publicKey
+        );
+        const instructionData = Buffer.from(
+            Uint8Array.of(
+                0,
+                ...new BN(minimumStakeAmount * 10**TOKEN_DECIMALS).toArray("le", 8),
+                ...new BN(minimumLockDuration).toArray("le", 8),
+                ...new BN(normalStakingApy * 10).toArray("le", 8),
+                ...new BN(lockedStakingApy * 10).toArray("le", 8),
+                ...new BN(earlyWithdrawalFee * 10).toArray("le", 8)
+            )
         )
+        const initIX = new TransactionInstruction({
+            programId,
+            keys: [
+                { pubkey: adminKeyPair.publicKey, isSigner: true, isWritable: false },
+                { pubkey: dataAccountPubKey, isSigner: false, isWritable: true },
+                { pubkey: tokenAccountKeypair.publicKey, isSigner: false, isWritable: true },
+                { pubkey: mintPubkey, isSigner: false, isWritable: false },
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false }
+            ],
+            data: instructionData
+        });
+        const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+        const messageV0 = new TransactionMessage({
+            payerKey: adminKeyPair.publicKey,
+            recentBlockhash: latestBlockHash.blockhash,
+            instructions: [createTokenAcctIX, initTokenAcctIX, initIX]
+        }).compileToV0Message();
+        const transaction = new VersionedTransaction(messageV0);
+        transaction.sign([adminKeyPair, tokenAccountKeypair]);
+        const txId = await connection.sendTransaction(transaction, {maxRetries: 5});
+        const confirmation = await connection.confirmTransaction({
+            signature: txId,
+            blockhash: latestBlockHash.blockhash,
+            lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+        });
+        if (confirmation.value.err) {
+            console.log(`Transaction could not be confirmed ❌❌❌`)
+        } else {
+            writePublicKey(tokenAccountKeypair.publicKey, 'token_acct', network);
+            writeSecretKey(tokenAccountKeypair.secretKey, 'token_acct', network);
+            writePublicKey(dataAccountPubKey, 'data_acct', network);
+            if (network == "devnet" || network == "localhost") {
+                await mintTo(
+                    connection,
+                    adminKeyPair,
+                    mintPubkey,
+                    tokenAccountKeypair.publicKey,
+                    adminKeyPair,
+                    100000*10**TOKEN_DECIMALS
+                )
+            }
+        }
     }
 }
 
@@ -223,5 +226,6 @@ setup(
     24*60*60,
     500,
     700,
-    1
+    1,
+    false
 ).then((val) => console.log(val))
