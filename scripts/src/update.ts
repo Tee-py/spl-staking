197669f5b2dc249ec4ff4d262598867bf8c0996f
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-03-20 20:50:06 +0100

    minor changes for network congestion

--- scripts/src/update.ts
+++ scripts/src/update.ts
@@ -1,7 +1,8 @@
 import {
     Connection,
     Keypair, PublicKey,
-    TransactionInstruction, TransactionMessage, VersionedTransaction
+    TransactionInstruction, TransactionMessage, VersionedTransaction,
+    ComputeBudgetProgram, Transaction
 } from "@solana/web3.js";
 import BN from "bn.js";
 import {getCluster, getKeypair} from "./utils";
@@ -15,7 +16,6 @@ const updateAPY = async (
     const cluster = getCluster(network);
     const connection = new Connection(cluster, "confirmed");
     const programId = new PublicKey(PROGRAM_ID);
-    let tokenAccountKeypair = new Keypair();
     let adminKeyPair: Keypair;
     try {
         adminKeyPair = getKeypair("admin", network);
@@ -24,6 +24,8 @@ const updateAPY = async (
         return
     }
 
+    //const PRIORITY_RATE = 100; // MICRO_LAMPORTS
+    //const PRIORITY_FEE_IX = ComputeBudgetProgram.setComputeUnitPrice({microLamports: PRIORITY_RATE});
     const dataAccountPubKey = new PublicKey("994NbZhmVGDAvXHWW8VMA4kBgeHpKF8xebncag4KnRVE");
     const instructionData = Buffer.from(
         Uint8Array.of(
@@ -40,29 +42,65 @@ const updateAPY = async (
         ],
         data: instructionData
     });
-    const latestBlockHash = await connection.getLatestBlockhash('confirmed');
-    const messageV0 = new TransactionMessage({
-        payerKey: adminKeyPair.publicKey,
-        recentBlockhash: latestBlockHash.blockhash,
-        instructions: [updateAPYIX]
-    }).compileToV0Message();
-    const transaction = new VersionedTransaction(messageV0);
-    transaction.sign([adminKeyPair, tokenAccountKeypair]);
-    const txId = await connection.sendTransaction(transaction, {maxRetries: 5});
-    const confirmation = await connection.confirmTransaction({
-        signature: txId,
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
-    });
-    if (confirmation.value.err) {
-        console.log(`Transaction could not be confirmed ❌❌❌`);
-    } else {
-        console.log(`Transaction confirmed successfully ✅✅✅`)
-    }
+    const txn = new Transaction();
+    txn.add(updateAPYIX);
+    const hash = await connection.sendTransaction(
+        txn, [adminKeyPair],
+        { skipPreflight: false, preflightCommitment: "confirmed"}
+    )
+    console.log(hash);
+    // const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    // const messageV0 = new TransactionMessage({
+    //     payerKey: adminKeyPair.publicKey,
+    //     recentBlockhash: latestBlockHash.blockhash,
+    //     instructions: [updateAPYIX]
+    // }).compileToV0Message();
+    // const transaction = new VersionedTransaction(messageV0);
+    // transaction.sign([adminKeyPair]);
+    // const txId = await connection.sendTransaction(transaction);
+    // console.log(`Finalizing Transaction... ${txId}`);
+    // const confirmation = await connection.confirmTransaction({
+    //     signature: txId,
+    //     blockhash: latestBlockHash.blockhash,
+    //     lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+    // });
+    // if (confirmation.value.err) {
+    //     console.log(`Transaction could not be confirmed ❌❌❌`);
+    // } else {
+    //     console.log(`Transaction confirmed successfully ✅✅✅`);
+    //     console.log(`Explorer Link: https://explorer.solana.com/tx/${txId}`);
+    // }
+    // const maxRetries = 5;
+    // let tries = 0;
+    // while (tries <= maxRetries) {
+    //     try {
+    //         const latestBlockHash = await connection.getLatestBlockhash('confirmed');
+    //         messageV0.recentBlockhash = latestBlockHash.blockhash;
+    //         const transaction = new VersionedTransaction(messageV0);
+    //         transaction.sign([adminKeyPair]);
+    //         const txId = await connection.sendTransaction(transaction, {maxRetries: 5});
+    //         const confirmation = await connection.confirmTransaction({
+    //             signature: txId,
+    //             blockhash: latestBlockHash.blockhash,
+    //             lastValidBlockHeight: latestBlockHash.lastValidBlockHeight
+    //         });
+    //         if (confirmation.value.err) {
+    //             console.log(`Transaction could not be confirmed ❌❌❌`);
+    //         } else {
+    //             console.log(`Transaction confirmed successfully ✅✅✅`);
+    //             console.log(`Explorer Link: https://explorer.solana.com/tx/${txId}`);
+    //             break
+    //         }
+    //     } catch (e) {
+    //         console.log(e)
+    //         tries++
+    //         console.log(`Could not confirm txn... ${maxRetries - tries} remaining`);
+    //     }
+    // }
 }
 
 updateAPY(
     "mainnet",
-    10,
-    7*24*60*60
+    1780,
+    3127
 ).then((val) => console.log(val))
